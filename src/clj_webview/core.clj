ab853424e4cf43f670a4be344e89c4d38cb55eff
Matthew Carter <m@ahungry.com>
2018-08-26 03:03:17 -0400

    closer

--- src/clj_webview/core.clj
+++ src/clj_webview/core.clj
@@ -4,8 +4,5 @@
   (:gen-class))
 
 (defn -main []
-  (l/async-load "http://ahungry.com" (b/boot))
-  ;; (l/async-load "http://ahungry.com" b/webengine)
-  )
-
-;; (defn -main [] "Ok")
+  (println "Starting version 0.0.0")
+  (l/async-load "http://ahungry.com" (b/boot)))
